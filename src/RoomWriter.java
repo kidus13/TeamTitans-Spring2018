1830a0595a97362bff0f01c0334c94a57e017ef9
TEtheridge7 <tetheridge@ggc.edu>
2018-04-07 00:24:12 -0400

    the rest of the text files in the class

--- src/RoomWriter.java
+++ src/RoomWriter.java
@@ -212,7 +212,7 @@ public class RoomWriter {
 }
 	private Scanner Room11;
 	
-	public void openRoom111() {
+	public void openRoom11() {
 		try {
 			Room11 = new Scanner(new File("room11.txt"));
 		} catch (Exception e) {
@@ -297,6 +297,7 @@ public class RoomWriter {
 			System.out.println("file 15 not found");
 		}
 	}
+
 	public void readRoom15() {
 		while (Room15.hasNextLine()) {
 			String _Room15 = Room15.nextLine();
@@ -305,9 +306,368 @@ public class RoomWriter {
 	}
 	public void closeRoom15() {
 		Room15.close();
-	
-	
+	}
+		private Scanner Room16;
+		
+		public void openRoom16() {
+			try {
+				Room16 = new Scanner(new File("room16.txt"));
+			} catch (Exception e) {
+				System.out.println("file 16 not found");
+			}
+		}
+		public void readRoom16() {
+			while (Room16.hasNextLine()) {
+				String _Room16 = Room16.nextLine();
+			System.out.println(_Room16);
+			}
+		}
+		public void closeRoom16() {
+			Room16.close();
 }
+private Scanner Room17;
+		
+		public void openRoom17() {
+			try {
+				Room17 = new Scanner(new File("room17.txt"));
+			} catch (Exception e) {
+				System.out.println("file 17 not found");
+			}
+		}
+		public void readRoom17() {
+			while (Room17.hasNextLine()) {
+				String _Room17 = Room17.nextLine();
+			System.out.println(_Room17);
+			}
+		}
+		public void closeRoom17() {
+			Room17.close();
+}
+private Scanner Room18;
+		
+		public void openRoom18() {
+			try {
+				Room18 = new Scanner(new File("room18.txt"));
+			} catch (Exception e) {
+				System.out.println("file 18 not found");
+			}
+		}
+		public void readRoom18() {
+			while (Room18.hasNextLine()) {
+				String _Room18 = Room18.nextLine();
+			System.out.println(_Room18);
+			}
+		}
+		public void closeRoom18() {
+			Room18.close();
+}
+private Scanner Room19;
+		
+		public void openRoom19() {
+			try {
+				Room19 = new Scanner(new File("room19.txt"));
+			} catch (Exception e) {
+				System.out.println("file 19 not found");
+			}
+		}
+		public void readRoom19() {
+			while (Room19.hasNextLine()) {
+				String _Room19 = Room19.nextLine();
+			System.out.println(_Room19);
+			}
+		}
+		public void closeRoom19() {
+			Room19.close();
+}
+private Scanner Room20;
+		
+		public void openRoom20() {
+			try {
+				Room20 = new Scanner(new File("room20.txt"));
+			} catch (Exception e) {
+				System.out.println("file  not found");
+			}
+		}
+		public void readRoom20() {
+			while (Room20.hasNextLine()) {
+				String _Room20 = Room20.nextLine();
+			System.out.println(_Room20);
+			}
+		}
+		public void closeRoom20() {
+			Room20.close();
+}
+private Scanner Room21;
+		
+		public void openRoom21() {
+			try {
+				Room21 = new Scanner(new File("room21.txt"));
+			} catch (Exception e) {
+				System.out.println("file 21 not found");
+			}
+		}
+		public void readRoom21() {
+			while (Room21.hasNextLine()) {
+				String _Room21 = Room21.nextLine();
+			System.out.println(_Room21);
+			}
+		}
+		public void closeRoom21() {
+			Room21.close();
+}
+private Scanner Room22;
+		
+		public void openRoom22() {
+			try {
+				Room22 = new Scanner(new File("room22.txt"));
+			} catch (Exception e) {
+				System.out.println("file 22 not found");
+			}
+		}
+		public void readRoom22() {
+			while (Room22.hasNextLine()) {
+				String _Room22 = Room22.nextLine();
+			System.out.println(_Room22);
+			}
+		}
+		public void closeRoom22() {
+			Room22.close();
+}
+private Scanner Room23;
+		
+		public void openRoom23() {
+			try {
+				Room23 = new Scanner(new File("room23.txt"));
+			} catch (Exception e) {
+				System.out.println("file 23 not found");
+			}
+		}
+		public void readRoom23() {
+			while (Room23.hasNextLine()) {
+				String _Room23 = Room23.nextLine();
+			System.out.println(_Room23);
+			}
+		}
+		public void closeRoom23() {
+			Room23.close();
+}
+private Scanner Room24;
+		
+		public void openRoom24() {
+			try {
+				Room24 = new Scanner(new File("room24.txt"));
+			} catch (Exception e) {
+				System.out.println("file 24 not found");
+			}
+		}
+		public void readRoom24() {
+			while (Room24.hasNextLine()) {
+				String _Room24 = Room24.nextLine();
+			System.out.println(_Room24);
+			}
+		}
+		public void closeRoom24() {
+			Room24.close();
+}
+private Scanner Room25;
+		
+		public void openRoom25() {
+			try {
+				Room25 = new Scanner(new File("room25.txt"));
+			} catch (Exception e) {
+				System.out.println("file 25 not found");
+			}
+		}
+		public void readRoom25() {
+			while (Room25.hasNextLine()) {
+				String _Room25 = Room25.nextLine();
+			System.out.println(_Room25);
+			}
+		}
+		public void closeRoom25() {
+			Room25   .close();
+}
+private Scanner Room26;
+		
+		public void openRoom26() {
+			try {
+				Room26 = new Scanner(new File("room26.txt"));
+			} catch (Exception e) {
+				System.out.println("file 26 not found");
+			}
+		}
+		public void readRoom26() {
+			while (Room26.hasNextLine()) {
+				String _Room26 = Room26.nextLine();
+			System.out.println(_Room26);
+			}
+		}
+		public void closeRoom26() {
+			Room26.close();
+}
+private Scanner Room27;
+		
+		public void openRoom27() {
+			try {
+				Room27 = new Scanner(new File("room27.txt"));
+			} catch (Exception e) {
+				System.out.println("file 27 not found");
+			}
+		}
+		public void readRoom27() {
+			while (Room27.hasNextLine()) {
+				String _Room27 = Room27.nextLine();
+			System.out.println(_Room27);
+			}
+		}
+		public void closeRoom27() {
+			Room27.close();
+}
+private Scanner Room28;
+		
+		public void openRoom28() {
+			try {
+				Room28 = new Scanner(new File("room28.txt"));
+			} catch (Exception e) {
+				System.out.println("file 28 not found");
+			}
+		}
+		public void readRoom28() {
+			while (Room28.hasNextLine()) {
+				String _Room28 = Room28.nextLine();
+			System.out.println(_Room28);
+			}
+		}
+		public void closeRoom28() {
+			Room28.close();
+}
+private Scanner Room29;
+		
+		public void openRoom29() {
+			try {
+				Room29 = new Scanner(new File("room29.txt"));
+			} catch (Exception e) {
+				System.out.println("file 29 not found");
+			}
+		}
+		public void readRoom29() {
+			while (Room29.hasNextLine()) {
+				String _Room29 = Room29.nextLine();
+			System.out.println(_Room29);
+			}
+		}
+		public void closeRoom29() {
+			Room29.close();
+}
+private Scanner Room30;
+		
+		public void openRoom30() {
+			try {
+				Room30 = new Scanner(new File("room30.txt"));
+			} catch (Exception e) {
+				System.out.println("file 30 not found");
+			}
+		}
+		public void readRoom30() {
+			while (Room30.hasNextLine()) {
+				String _Room30 = Room30.nextLine();
+			System.out.println(_Room30);
+			}
+		}
+		public void closeRoom30() {
+			Room30.close();
+}
+private Scanner Room31;
+		
+		public void openRoom31() {
+			try {
+				Room31 = new Scanner(new File("room31.txt"));
+			} catch (Exception e) {
+				System.out.println("file 31 not found");
+			}
+		}
+		public void readRoom31() {
+			while (Room31.hasNextLine()) {
+				String _Room31 = Room31.nextLine();
+			System.out.println(_Room31);
+			}
+		}
+		public void closeRoom31() {
+			Room31.close();
+}
+private Scanner Room32;
+		
+		public void openRoom32() {
+			try {
+				Room32 = new Scanner(new File("room32.txt"));
+			} catch (Exception e) {
+				System.out.println("file 32 not found");
+			}
+		}
+		public void readRoom32() {
+			while (Room32.hasNextLine()) {
+				String _Room32 = Room32.nextLine();
+			System.out.println(_Room32);
+			}
+		}
+		public void closeRoom32() {
+			Room32.close();
+}
+private Scanner Room33;
+		
+		public void openRoom33() {
+			try {
+				Room33 = new Scanner(new File("room33.txt"));
+			} catch (Exception e) {
+				System.out.println("file 33 not found");
+			}
+		}
+		public void readRoom33() {
+			while (Room33.hasNextLine()) {
+				String _Room33 = Room33.nextLine();
+			System.out.println(_Room33);
+			}
+		}
+		public void closeRoom33() {
+			Room33.close();
+}
+private Scanner Room34;
+		
+		public void openRoom34() {
+			try {
+				Room34 = new Scanner(new File("room34.txt"));
+			} catch (Exception e) {
+				System.out.println("file 34 not found");
+			}
+		}
+		public void readRoom34() {
+			while (Room34.hasNextLine()) {
+				String _Room34 = Room34.nextLine();
+			System.out.println(_Room34);
+			}
+		}
+		public void closeRoom34() {
+			Room34.close();
+}
+private Scanner Room35;
+		
+		public void openRoom35() {
+			try {
+				Room35 = new Scanner(new File("room35.txt"));
+			} catch (Exception e) {
+				System.out.println("file 35 not found");
+			}
+		}
+		public void readRoom35() {
+			while (Room35.hasNextLine()) {
+				String _Room35 = Room35.nextLine();
+			System.out.println(_Room35);
+			}
+		}
+		public void closeRoom35() {
+			Room35.close();
+}
+		
 /*	private Scanner Room;
 	
 	public void openRoom() {
