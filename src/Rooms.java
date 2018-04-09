ca345017c49b033c904b2a970010fb0f4f421693
TEtheridge7 <tetheridge@ggc.edu>
2018-04-09 18:47:50 -0400

    Updates to Rooms class

--- src/Rooms.java
+++ src/Rooms.java
@@ -26,7 +26,7 @@ public class Rooms {
 		
 		}
 
-	public Rooms(String roomID, String roomName, String description, int doorID, int attribute4) {
+	public Rooms(String roomID, String roomName, String description, int doorID) {
 		this.roomID = roomID;
 		this.roomName = roomName;
 		this.description = description;
@@ -47,37 +47,47 @@ public class Rooms {
 		//should the roomid be changed to string since each level starts with that levels ID?
 		
 		//need to fix these, theyre out of order 4.6 Trivon
+		Rooms Entrance = new Rooms("ER01", "Entrance", null, 0);
+		Rooms GuardiansStation = new Rooms("ER02", "Guardian's Station", null, 1);
+		Rooms GuardsBarracks = new Rooms("ER03", "Guard's Barracks", null, 2);
+		Rooms LongHallway = new Rooms("ER04", "Long Hallway", null, 3);
+		Rooms TortureRoom = new Rooms("ER05", "Torture Room", null, 4);
+		Rooms Cell = new Rooms("ER06", "Cell", null, 5);
+		Rooms EquipmentRoom = new Rooms("ER07", "Equipment Room", null, 6);
+		Rooms LargeChamber = new Rooms("ER08", "Large Chamber", null, 7);
+		
 		//Rooms EntranceRoom = new Rooms("WR01", "Entrance Room", room1, 1, 1);
-		Rooms VerKsrRoom = new Rooms("WR02", "VerKsr Room", null, 2, 2); //verksr is room 10 actually
-		Rooms SarCoRoom = new Rooms("WR03", "SarCo Room", null, 3, 0 );
-		Rooms StalacticeRoom = new Rooms("WR04", "Stalactice Room", null, 4, 4);
-		Rooms CraftRoomStore= new Rooms("WR05", "Craft Store Room", null, 5, 5);
-		/*Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0);
+		Rooms EntranceRoom = new Rooms("WR01", "Entrance Room", null, 8);
+		Rooms VerKsrRoom = new Rooms("WR02", "VerKsr Room", null, 9); //verksr is room 10 actually
+		Rooms SarCoRoom = new Rooms("WR03", "SarCo Room", null, 10);
+		Rooms StalacticeRoom = new Rooms("WR04", "Stalactice Room", null, 11);
+		Rooms CraftRoomStore= new Rooms("WR05", "Craft Store Room", null, 12);
+		Rooms DoomsRoom = new Rooms("WR06", "Dooms Room", null, 13);
+		Rooms HellPathRoom = new Rooms("WR07", "Hell Path Room", null, 14);
+		Rooms WitchOfTheFallsRoom = new Rooms("WR08", "Witch of the Falls Room", null, 15);
+		Rooms FireHallway = new Rooms("FH", "Fire Hallway", null, 16);
+		Rooms FireCavern1 = new Rooms("FR01", "Fire Cavern 1", null, 17);
+		Rooms FireCavern2 = new Rooms("FR02", "Fire Cavern 2", null, 18);
+		Rooms FireCavern3 = new Rooms("FR03", "Fire Cavern 3", null, 19);
+		Rooms FireCavern4 = new Rooms("FR04", "Fire Cavern 4", null, 20);
+		Rooms FireCavern5 = new Rooms("FR05", "Fire Cavern 5", null, 21);
+		Rooms FireCavern6 = new Rooms("FR06", "Fire Cavern 6", null, 22);
+		Rooms FireCavern7 = new Rooms("FR07", "Fire Cavern 7", null, 23);
+		Rooms FireCavern8 = new Rooms("FR08", "Fire Cavern 8", null, 24);
+		Rooms FireCavern9 = new Rooms("FR09", "Fire Cavern 9", null, 25);
+		Rooms FireCavern10= new Rooms("FR10", "Fire Cavern 10", null, 26);
+		Rooms = new Rooms("", "", "", 27);
+		Rooms = new Rooms("", "", "", 28);
+		Rooms = new Rooms("", "", "", 29);
+		Rooms = new Rooms("", "", "", 30);
+		Rooms = new Rooms("", "", "", 31);
+		Rooms = new Rooms("", "", "", 32);
+		Rooms = new Rooms("", "", "", 33);
+		Rooms = new Rooms("", "", "", 34);
+		Rooms = new Rooms("", "", "", 35);
 		Rooms = new Rooms("", "",3 "", 0, 0);
 		Rooms = new Rooms("", "", "", 0, 0);
-		Rooms = new Rooms("", "", "", 0, 0)*/;
+		Rooms = new Rooms("", "", "", 0, 0);
 		
 	}
 
