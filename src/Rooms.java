6ce7e14a62ef67467a6abdeb155c5f69985eefbf
TEtheridge7 <tetheridge@ggc.edu>
2018-04-10 23:14:24 -0400

    NEed to research to add to rooms

--- src/Rooms.java
+++ src/Rooms.java
@@ -4,7 +4,15 @@
  */
 public class Rooms {
 
+	public boolean isDoorOpen() {
+		int doorOpen = 0;
+		if (doorOpen <= 0) {
+			return true;
+		}
+		return false;
+	}
 
+	//ummmmm need to look into this more because i know im missing something so let me look it up - 4.10
 }	  
 			  
 
