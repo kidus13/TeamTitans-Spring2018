7e2785187540d6e728b1ca7552368b898b02ad01
WilliamPridgen <wpridgen@ggc.edu>
2018-04-15 11:09:53 -0400

    corrected player

--- src/Player.java
+++ src/Player.java
@@ -3,243 +3,278 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Player {
-public static void main(String[] args) {
 
-RoomWriter r = new RoomWriter();
-// Weapon w = new Weapon(null, 0, 0, 0, false, false, false, false);
-Scanner input = new Scanner(System.in);
-boolean inRoom = false;
-List<Weapon> inv = new ArrayList<Weapon>();
-Scanner inputItem = new Scanner(System.in);
+	public static void main(String[] args) {
 
-System.out.println("Hello user please enter in the room you would like to enter");
-String userInput = input.nextLine();
-int room = 0;
+		RoomWriter r = new RoomWriter();
+		Character p = new Character("", 0, 0);
+		Monster m = new Monster("", 0, 0);
+		Weapon w = new Weapon("", 0, 0, 0, false, false, false, false);
+		Battle b = new Battle();
+ 
+		Scanner input = new Scanner(System.in);
+		boolean inRoom = false;
 
-while (true) {
+		// Add inventory to array
+		List<Weapon> inv = new ArrayList<Weapon>();
+		Scanner inputItem = new Scanner(System.in);
 
-if (userInput.contains("north")) {
-room++;
-}
-if (userInput.contains("south")) {
-room--;
-}
+		System.out.println("Hello user please enter in the room you would like to enter");
+		String userInput = input.nextLine();
+		int room = 0;
 
-// room1
-if (room == 1) {
-r.openRoom1();
-r.readRoom1();
-r.closeRoom1();
-userInput = input.nextLine();
+		while (true) {
 
-}
-// room2
-if (room == 2) {
-r.openRoom2();
-r.readRoom2();
-r.closeRoom2();
-userInput = input.nextLine();
+			if (userInput.contains("north")) {
+				room++;
+			}
+			if (userInput.contains("south")) {
+				room--;
+			}
 
-System.out.println("Guard's Armor and Sword are availble to pick up! ");
-System.out.println("Would you like to pick up items: YES or NO");
-String choice = inputItem.nextLine();
+			// room1
+			if (room == 1) {
+				r.openRoom1();
+				r.readRoom1();
+				r.closeRoom1();
+				// userInput = input.nextLine();
+				if (userInput.contains("north")) {
+					room = 2;
 
-if (choice.equalsIgnoreCase("yes")) {
-inv.add(Weapon.getEI05());
-System.out.println(inv + "\n");
-}
+				}
 
-}
-// room3
-if (room == 3) {
-r.openRoom3();
-r.readRoom3();
-r.closeRoom3();
-userInput = input.nextLine();
+			}
+			// room2
+			if (room == 2) {
+				// userInput = input.nextLine();
+				System.out.println("You are now in room 2");
+				System.out.println("Would you like to search room? yes or no ");
+				userInput = input.nextLine();
+				if (userInput.contains("yes")) {
+					r.openRoom2();
+					r.readRoom2();
+					r.closeRoom2();
+					System.out.println("");
+					System.out.println("There is a monster lurking in this room");
+					System.out.println("Would you like to examine? yes or no");
+					userInput = input.nextLine();
+					if (userInput.contains("yes")) {
+						m.getEM01();
 
-System.out.println("something available! ");
-System.out.println("Would you like to pick up items: YES or NO");
-String choice = inputItem.nextLine();
+						System.out.println("The guard has a few weapons you might need");
+						System.out.println("Would you like to see what he has? yes or no");
+						userInput = input.nextLine();
+						if (userInput.contains("yes")) {
+							w.getEI01();
+							w.getEI02();
+							System.out.println("You have to battle the monster and defeat him to get the items");
+							System.out.println("Would you like to fight or flee monster?");
+							userInput = input.nextLine();
+							if (userInput.contains("fight")) {
+								b.battleMonster(m.getWM04()); // Still need to figure this out 
+								m.getWM04();
+								System.out.println("You won your battle!");
+								System.out.println("You can now collect items dropped by the monster");
+								System.out.println("Would you like to add items to your inventory? yes or no");
+								String choice = inputItem.nextLine();
+								if (choice.equalsIgnoreCase("yes")) {
+									for(int i = 0; i<inv.size(); i++){
+									if(inv.get(i)==null)
+									inv.add(Weapon.getEI05());
+									}
+									System.out.println(inv + "\n");
+							}
+						}
 
-if (choice.equalsIgnoreCase("yes")) {
-inv.add(Weapon.getEI06());
-System.out.println("\n" + inv + "\n");
-}
+					} else if (userInput.contains("no")) {
+						System.out.println("Be careful a monster could pop out of a corner");
+					}
 
-}
-// room4
-if (room == 4) {
-r.openRoom4();
-r.readRoom4();
-r.closeRoom4();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-System.out.println("Do you want to access room 5 or 7?");
-userInput = input.nextLine();
-if (userInput.contains("5")) {
-room = 5;
-}
-if (userInput.contains("7")) {
-room = 7;
-}
-}
-}
+				}
 
-// room5
-if (room == 5) {
-r.openRoom5();
-r.readRoom5();
-r.closeRoom5();
-userInput = input.nextLine();
+				// room3
+				if (room == 3) {
+					r.openRoom3();
+					r.readRoom3();
+					r.closeRoom3();
+					userInput = input.nextLine();
 
-}
-// room6
-if (room == 6) {
-r.openRoom6();
-r.readRoom6();
-r.closeRoom6();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-room = 6;
-System.out.println("Room 7 cannot be accessed in this room");
-System.out.println();
-userInput = input.nextLine();
+				}
+				// room4
+				if (room == 4) {
+					r.openRoom4();
+					r.readRoom4();
+					r.closeRoom4();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						System.out.println("Do you want to access room 5 or 7?");
+						userInput = input.nextLine();
+						if (userInput.contains("5")) {
+							room = 5;
+						}
+						if (userInput.contains("7")) {
+							room = 7;
+						}
+					}
+				}
 
-}
-}
-// room7
-if (room == 7) {
-r.openRoom7();
-r.readRoom7();
-r.closeRoom7();
-userInput = input.nextLine();
-if (userInput.contains("south")) {
-room = 5;
-}
-}
-// room8
-if (room == 8) {
-r.openRoom8();
-r.readRoom8();
-r.closeRoom8();
-userInput = input.nextLine();
-}
+				// room5
+				if (room == 5) {
+					r.openRoom5();
+					r.readRoom5();
+					r.closeRoom5();
+					userInput = input.nextLine();
 
-// hallway1
-if (userInput.contains("hallway1")) {
-{
-System.out.println("entering hallway1");
-}
-}
+				}
+				// room6
+				if (room == 6) {
+					r.openRoom6();
+					r.readRoom6();
+					r.closeRoom6();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						room = 6;
+						System.out.println("Room 7 cannot be accessed in this room");
+						System.out.println();
+						userInput = input.nextLine();
 
-// hallway2
-if (userInput.contains("hallway2")) {
-{
-System.out.println("entering hallway2");
-}
-}
+					}
+				}
+				// room7
+				if (room == 7) {
+					r.openRoom7();
+					r.readRoom7();
+					r.closeRoom7();
+					userInput = input.nextLine();
+					if (userInput.contains("south")) {
+						room = 5;
+					}
+				}
+				// room8
+				if (room == 8) {
+					r.openRoom8();
+					r.readRoom8();
+					r.closeRoom8();
+					userInput = input.nextLine();
+				}
 
-// room9
-if (room == 9) {
-r.openRoom9();
-r.readRoom9();
-r.closeRoom9();
-userInput = input.nextLine();
-}
+				// hallway1
+				if (userInput.contains("hallway1")) {
+					{
+						System.out.println("entering hallway1");
+					}
+				}
 
-// room10
-if (room == 10) {
-r.openRoom10();
-r.readRoom10();
-r.closeRoom10();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-System.out.println("Do you want to access room 11 or 13?");
-userInput = input.nextLine();
-if (userInput.contains("11")) {
-room = 11;
-}
-if (userInput.contains("13")) {
-room = 13;
-}
-}
-}
+				// hallway2
+				if (userInput.contains("hallway2")) {
+					{
+						System.out.println("entering hallway2");
+					}
+				}
 
-// room11
-if (room == 11) {
-r.openRoom11();
-r.readRoom11();
-r.closeRoom11();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-System.out.println("Do you want to access room 12 or hallway?");
-userInput = input.nextLine();
-if (userInput.contains("12")) {
-room = 12;
-}
-}
-}
+				// room9
+				if (room == 9) {
+					r.openRoom9();
+					r.readRoom9();
+					r.closeRoom9();
+					userInput = input.nextLine();
+				}
 
-// room12
-if (room == 12) {
-r.openRoom12();
-r.readRoom12();
-r.closeRoom12();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-System.out.println("Do you want to access room 13, room  14, hallway 1 or hallway2?");
-userInput = input.nextLine();
-if (userInput.contains("13")) {
-room = 13;
-}
-if (userInput.contains("14")) {
-room = 14;
-}
-}
-}
+				// room10
+				if (room == 10) {
+					r.openRoom10();
+					r.readRoom10();
+					r.closeRoom10();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						System.out.println("Do you want to access room 11 or 13?");
+						userInput = input.nextLine();
+						if (userInput.contains("11")) {
+							room = 11;
+						}
+						if (userInput.contains("13")) {
+							room = 13;
+						}
+					}
+				}
 
-// room13
-if (room == 13) {
-r.openRoom13();
-r.readRoom13();
-r.closeRoom13();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-System.out.println("Do you want to access room 15 or hallway2?");
-userInput = input.nextLine();
-if (userInput.contains("15")) {
-room = 15;
-}
-}
-}
+				// room11
+				if (room == 11) {
+					r.openRoom11();
+					r.readRoom11();
+					r.closeRoom11();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						System.out.println("Do you want to access room 12 or hallway?");
+						userInput = input.nextLine();
+						if (userInput.contains("12")) {
+							room = 12;
+						}
+					}
+				}
 
-// room14
-if (room == 14) {
-r.openRoom14();
-r.readRoom14();
-r.closeRoom14();
-userInput = input.nextLine();
-if (userInput.contains("north")) {
-room = 16;
+				// room12
+				if (room == 12) {
+					r.openRoom12();
+					r.readRoom12();
+					r.closeRoom12();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						System.out.println("Do you want to access room 13, room  14, hallway 1 or hallway2?");
+						userInput = input.nextLine();
+						if (userInput.contains("13")) {
+							room = 13;
+						}
+						if (userInput.contains("14")) {
+							room = 14;
+						}
+					}
+				}
 
-}
-}
+				// room13
+				if (room == 13) {
+					r.openRoom13();
+					r.readRoom13();
+					r.closeRoom13();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						System.out.println("Do you want to access room 15 or hallway2?");
+						userInput = input.nextLine();
+						if (userInput.contains("15")) {
+							room = 15;
+						}
+					}
+				}
 
-// room15
-if (room == 15) {
-r.openRoom15();
-r.readRoom15();
-r.closeRoom15();
-userInput = input.nextLine();
-}
+				// room14
+				if (room == 14) {
+					r.openRoom14();
+					r.readRoom14();
+					r.closeRoom14();
+					userInput = input.nextLine();
+					if (userInput.contains("north")) {
+						room = 16;
 
-// room16
-if (room == 16) {
-r.openRoom16();
-r.readRoom16();
-r.closeRoom16();
-userInput = input.nextLine();
-}
-}
+					}
+				}
+
+				// room15
+				if (room == 15) {
+					r.openRoom15();
+					r.readRoom15();
+					r.closeRoom15();
+					userInput = input.nextLine();
+				}
+
+				// room16
+				if (room == 16) {
+					r.openRoom16();
+					r.readRoom16();
+					r.closeRoom16();
+					userInput = input.nextLine();
+				}
+			}
+		}
+	}
 }
 }
