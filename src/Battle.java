c2509df43495357c19e20a43d111d9ae214a4c95
kidus13 <kdebesai1@ggc.edu>
2018-04-16 18:53:22 -0400

    Battle class 90% figured out

--- src/Battle.java
+++ src/Battle.java
@@ -3,105 +3,163 @@ import java.util.Scanner;
 
 public class Battle {
 	Weapon w = new Weapon("", 0, 0, 0, 0, 0, false, false, false, false);
-	Character c = new Character(getName(), 0, 0);
+	Character c = new Character("", 0, 0);
 	Monster m = new Monster(" ", 0, 0);
-
-	int playerAttackPoints = 10;
 	
-	// Player attacks monster
-	public void attack(Monster _monster) {
-		int weaponDamage = w.getAI01().getDamage();
-		int attackPoints = c.getPlayerAttackPoints();
-		int monsterHP = _monster.getHealthPoints();
-		int decreaseHealthPoints = monsterHP - (weaponDamage + attackPoints);		
-		System.out.println(decreaseHealthPoints);
-	}
+	
+    private int enemyHealth = (int) (Math.random()*75);
+    private int maxEnemyDamage = 25;
+    private String enemyName;
+    //player variables
+    private int playerHealth = 100;
+    private int playerDamage = 50;
+    //health potion variables
+    private int numHealthPotions = 3;
+    private int healAmount = 30;
+    private int dropChance = 50; //percentage
 
-	public boolean isPlayerDefeated(Character _player) {
-		int playerHP = this.c.getPlayerHP();
-		if (playerHP <= 0) {
-			return true;
-		}
-		return false;
-	}
+    public String getEnemyName() {
+        String[] enemy = {"Zombie", "Witch", "Hell Dog", "Skeleton"};
+        int i = (int) (Math.random() * 4);
+        this.enemyName = enemy[i];
+        return this.enemyName;
+    }
+    public int getEnemyHealth(){
+        return enemyHealth;
+    }
+    public void enemyAppears(){
+        String enemy = getEnemyName();
+        System.out.println("A " + enemy + " has appeared with " + enemyHealth + " health.");
+    }
+    public void attack() {
+        int damageTaken =(int) (Math.random() * maxEnemyDamage);
+        int damageDealt =(int) (Math.random() * playerDamage);
+        enemyHealth -= damageDealt - getEnemyHealth();
+        playerHealth -= damageTaken;
+        System.out.println("You strike for " + damageDealt);
+        System.out.println("You have been hit for " + damageTaken);
+        System.out.println("You have " + playerHealth + " hp left.\n The enemy has " + enemyHealth + " hp left.");
+        if (playerHealth < 0){
+            System.out.println("You died.");
+            System.exit(15);
+        }
+        if (enemyHealth < 0) {
+        	System.out.println("enemy is dead, pick up itmes?");
+        	
+        }
+    }
+        public void heal(){
+           
+            System.out.println("healed");
+        }
+        public void run(){
+            
+            System.out.println("bitch");
+        }
+        
+    }
 
-	private String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	
+	
+	
 
-	public void flee(Monster _monster) {
-		int attack = 0;
-		int monsterHP = _monster.getHealthPoints();
-		_monster.decreaseHealthPoints(attack);
-		System.out.println(monsterHP);
-
-	}
-
-	// player battles monster
-	public void battleMonster(Monster _monster) {
-		Random randomNums = new Random();
-		Scanner keyboard = new Scanner(System.in);
-		String room;
-		Scanner input = new Scanner(System.in);
-		room = input.nextLine();
-		RoomWriter r = new RoomWriter();
-
-		System.out.printf("***%s vs The Guard Monster ***\n", getName());
-
-		while (_monster.getHealthPoints() > 0) {
-			System.out.println("Choose your action:\n1. Attack\n2. Flee\n");
-			System.out.print("What would you like to do: ");
-			int playerActionChoice = keyboard.nextInt();
-			System.out.println();
-
-			switch (playerActionChoice) {
-			case 1: // Attack
-				this.attack(_monster);
-				this.attack(_monster);
-				_monster.decreaseHealthPoints(_monster.getHealthPoints());
-				System.out.printf("The %s's The Monster's HP is now 0!\n\n", getName());
-				break;
-			case 2: // Flee
-				this.flee(_monster);
-				System.out.println("Wuss... You're fleeing the monster");
-				System.out.println("Where would you like to flee to");
-				System.out.println("1. Room 1 \n 2. Room 2 \n 3. Room 3");
-
-				if (room.equals("1")) {
-					r.openRoom1();
-					r.readRoom1();
-					r.closeRoom1();
-					room = input.nextLine();
-
-					if (room.equals("2")) {
-						System.out.println("You are now leaving room 1");
-						System.out.println();
-					}
-					if (!room.equals("2") && !room.equals("1")) {
-						System.out.println("Sorry. You cannot skip from room 1 to " + room);
-						System.out.println("Try another room");
-						room = input.nextLine();
-					}
-
-				}
-				System.out.printf("\nInvalid input.  is not an available option.\n\n", playerActionChoice);
-				System.exit(0);
-			}
-
-			if (_monster.getHealthPoints() <= 0) {
-				break;
-			}
-			// Player.attack(this);
-			// end while
-
-			if (this.isPlayerDefeated(c)) {
-				System.out.printf("--%s You Lost!! ", getName());
-				System.exit(0);
-
-			}
-			break;
-		}
-	}
-
-}
+//	int playerAttackPoints = 10;
+//	
+//	// Player attacks monster
+//	public void attack(Monster _monster) {
+//		int weaponDamage = w.getAI01().getDamage();
+//		int attackPoints = c.getPlayerAttackPoints();
+//		int monsterHP = _monster.getHealthPoints();
+//		int decreaseHealthPoints = monsterHP - (weaponDamage + attackPoints);		
+//		System.out.println(decreaseHealthPoints);
+//	}
+//
+//	public boolean isPlayerDefeated(Character _player) {
+//		int playerHP = this.c.getPlayerHP();
+//		if (playerHP <= 0) {
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	private String getName() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	public void flee(Monster _monster) {
+//		int attack = 0;
+//		int monsterHP = _monster.getHealthPoints();
+//		_monster.decreaseHealthPoints(attack);
+//		System.out.println(monsterHP);
+//
+//	}
+//
+//	// player battles monster
+//	public void battleMonster(Monster _monster) {
+//		Random randomNums = new Random();
+//		Scanner keyboard = new Scanner(System.in);
+//		String room;
+//		Scanner input = new Scanner(System.in);
+//		room = input.nextLine();
+//		RoomWriter r = new RoomWriter();
+//
+//		System.out.printf("***%s vs The Guard Monster ***\n", getName());
+//
+//		while (_monster.getHealthPoints() > 0) {
+//			System.out.println("Choose your action:\n1. Attack\n2. Flee\n");
+//			System.out.print("What would you like to do: ");
+//			int playerActionChoice = keyboard.nextInt();
+//			System.out.println();
+//
+//			switch (playerActionChoice) {
+//			case 1: // Attack
+//				this.attack(_monster);
+//				this.attack(_monster);
+//				_monster.decreaseHealthPoints(_monster.getHealthPoints());
+//				System.out.printf("The %s's The Monster's HP is now 0!\n\n", getName());
+//				break;
+//			case 2: // Flee
+//				this.flee(_monster);
+//				System.out.println("Wuss... You're fleeing the monster");
+//				System.out.println("Where would you like to flee to");
+//				System.out.println("1. Room 1 \n 2. Room 2 \n 3. Room 3");
+//
+//				if (room.equals("1")) {
+//					r.openRoom1();
+//					r.readRoom1();
+//					r.closeRoom1();
+//					room = input.nextLine();
+//
+//					if (room.equals("2")) {
+//						System.out.println("You are now leaving room 1");
+//						System.out.println();
+//					}
+//					if (!room.equals("2") && !room.equals("1")) {
+//						System.out.println("Sorry. You cannot skip from room 1 to " + room);
+//						System.out.println("Try another room");
+//						room = input.nextLine();
+//					}
+//
+//				}
+//				System.out.printf("\nInvalid input.  is not an available option.\n\n", playerActionChoice);
+//				System.exit(0);
+//			}
+//
+//			if (_monster.getHealthPoints() <= 0) {
+//				break;
+//			}
+//			// Player.attack(this);
+//			// end while
+//
+//			if (this.isPlayerDefeated(c)) {
+//				System.out.printf("--%s You Lost!! ", getName());
+//				System.exit(0);
+//
+//			}
+//			break;
+//		}
+//	}
+//
+//}
--- src/Test.java
+++ src/Test.java
@@ -11,52 +11,24 @@ public class Test {
 		Battle b = new Battle();
 		WeaponsWriter wr = new WeaponsWriter();
 		
+		    Scanner input = new Scanner(System.in);
+		    System.out.println("Welcome to the dungeon");
+		    boolean running = true;
+		     while(running){
 
-		Scanner keyboard = new Scanner(System.in);
-		while (m.getWM04().getMonsterHP() > 0) {
-			System.out.println("Choose your action:\n1. Attack\n2. Flee\n");
-			System.out.print("What would you like to do: ");
-			int playerActionChoice = keyboard.nextInt();
-			//System.out.println();
+		        b.enemyAppears();
 
-			switch (playerActionChoice) {
-			case 1: // Attack
-				b.attack(m.getWM04());
-				b.attack(m.getWM04());
-				m.getWM04().decreaseMPH(10000);
-				System.out.println(m.getWM04().isDefeated());
-				break;
-//			case 2: // Flee
-//				b.flee(m.getWM04());
-//				System.out.println("Wuss... You're fleeing the monster");
-//				System.out.println("Where would you like to flee to");
-//				System.out.println("1. Room 1 \n 2. Room 2 \n 3. Room 3");
-//				int roomKeyboard = keyboard.nextInt();
-//				
-//				switch(roomKeyboard) {
-//				case 1: r =  {
-//					r.openRoom1();
-//					r.readRoom1();
-//					r.closeRoom1();
-//					
-//				}
-//					if (r.equals(2)) {
-//						System.out.println("You are now leaving room 1");
-//						System.out.println();
-//					}
-//					if (!r.equals(2) && !r.equals(1)) {
-//						System.out.println("Sorry. You cannot skip from room 1 to " + r);
-//						System.out.println("Try another room");
-//											}
-				}
-				
-				System.out.printf("\nInvalid input.  is not an available option.\n\n", playerActionChoice);
-				System.exit(0);
+		        System.out.println("Choose:\n1. To Attack \n2. To Heal \n3. To Run");
+		        String choice = input.next();
+		        if(choice.equals("1")) {
+		            b.attack();
+	
+		        } else if (choice.equals("2")){
+		            b.heal();
+		        } else {
+		            b.run();
+		            break;
+		        }
+		    }
 		}
-
-			if (m.getWM04().getMonsterHP() <= 0) {
-				System.out.println("Monster is dead");
-			
-			}
-	}
 }
